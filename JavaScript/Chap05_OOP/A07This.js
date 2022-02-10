83a3bef773a01d380088d01729e88dc0412b75ac
pxp13710 <pxp13716@daum.net>
2022-02-10 09:50:30 +0900

    .

--- JavaScript/Chap05_OOP/A07This.js
+++ JavaScript/Chap05_OOP/A07This.js
@@ -2,18 +2,14 @@
 console.log('---------------- this -----------------');
 
 // 객체 내부 메소드 내부의 함수.
-var count = 100;
+let count = 100;
 
-var myObj = {
-	
+const myObj = {
+	count: 0,
+
 	visit: function(){ 
-        
-
-        function inner(x) {
-            
-        }
-
-        inner(2);
+        ++this.count;
+        console.log('visit: ', this.count);
 	}
 };
 
@@ -22,13 +18,13 @@ console.log('');
 
 
 console.log('--------------- 편법 that ---------------');
-var count = 100;
+// var count = 100;
 
 
 
 
 console.log('--------------- 함수 내부의 함수 ---------------');
-var cnt = 10;
+// var cnt = 10;
 
 
 
