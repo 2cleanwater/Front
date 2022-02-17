036ab826aea3ec42eb5d968b280c2f8acbbb9d8c
pxp13710 <pxp13716@daum.net>
2022-02-17 09:59:34 +0900

    .

--- React/chap01component/src/components/A05Hook.js
+++ React/chap01component/src/components/A05Hook.js
@@ -1,19 +1,52 @@
 
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const A05Hook = () => {
 
     const [data, setData] = useState({
         num: 0,
-        str: ''
+        str: '',
+        name: ''
     });
-    const changeData = (evt) => setData( {...data, [evt.target.name]: evt.target.value} )
-
     const [today, setToday] = useState(new Date());
+
+    // 값이 유지되는 변수를 정의 할 목적으로 사용한다. 화면 갱신 작업은 이루어지지 않는다.
+    const cnt = useRef(1);
+
+    // DOM 객체를 참조할 목적으로도 사용한다
+    const inputElem = useRef();
+
 
     // setTimeout( () => {
     //     setToday(new Date());
     // }, 3000);
+    useEffect(() => {
+        setTimeout(()=> {
+            setToday(new Date());
+        }, 3000);
+    }, [data]);
+
+    useEffect(()=> {
+        console.log(inputElem.current);         // JavaScript 객체
+        inputElem.current.style.border = '1px solid orange';
+    }, []);
+
+
+    // 함수
+    const changeData = useCallback( 
+        (evt) => setData( {...data, [evt.target.name]: evt.target.value} )
+    , []);
+    const increase = useCallback( () => {
+        cnt.current++;
+        console.log(cnt)
+    }, []);
+    const decrease = useCallback( () => {
+        cnt.current--;
+        console.log(cnt)
+    }, []);
+    const changeName = useCallback( () => 
+        setData( {...data, name: inputElem.current.value} )
+    , [data]);
 
     return (
         <div>
@@ -30,10 +63,16 @@ const A05Hook = () => {
                 Today: {today.toLocaleTimeString()}<br />
                 <br />
 
-                Avg: 
+                Cnt: {cnt.current} <br />
+                <button className="btn btn-outline-primary btn-sm" onClick={increase}>+</button>
+                <button className="btn btn-outline-primary btn-sm" onClick={decrease}>-</button>
+                <br />
+
+
+                Avg: {data.name}
                     <div className="input-group">
-                        <input type="text" name="str" className="form-control"/>
-                        <button className="btn btn-outline-primary btn-sm">ADD</button>
+                        <input type="text" name="str" className="form-control" ref={inputElem}/>
+                        <button className="btn btn-outline-primary btn-sm" onClick={changeName}>ADD</button>
                     </div>
                     
                     
