8c0115ea74ce52a3dd2703ce85e9285e95ce51ff
pxp13710 <pxp13716@daum.net>
2022-02-17 15:48:40 +0900

    .

--- React/chap03_router/src/App.js
+++ React/chap03_router/src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 // npm i react-router react-router-dom
 // npm i react-loader-spinner
 
+import { Route, } from 'react-router-dom'
+
 function App() {
   const user = {name: 'HungBu', age: 20};
   const ary = ['a', 'b', 'c'];
@@ -13,7 +15,13 @@ function App() {
 
   return (
     <div className="card-body">
-      
+      <h1>Router</h1>
+
+      {/* Route가 기술된 영역에 지정한 컴퍼넌트가 표시된다 */}
+      <Route path='/index'    render={ () => <div>
+                                        <h3>Index Page</h3>
+                                        <div>Hello World</div>
+                                      </div>} />
     </div>
   );
 }
