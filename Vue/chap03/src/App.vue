40f606155f3f3eb5d4534d8170eddce193e639bf
pxp13710 <pxp13716@daum.net>
2022-02-21 11:52:12 +0900

    .

--- Vue/chap03/src/App.vue
+++ Vue/chap03/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Chap03 Component</h1>
+      </div>
+
+      <div class="col-12">
+        <A03Style></A03Style>
+      </div>
+
+      
+      <div class="col-6">
+        <A01Bind></A01Bind>
+      </div>
+      <div class="col-6">
+        <A02Attr></A02Attr>
+      </div>
+
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './../node_modules/bootstrap/dist/css/bootstrap.css'
+import A01Bind from './components/A01Bind.vue'
+import A02Attr from './components/A02Attribute.vue'
+import A03Style from './components/A03Style.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { A01Bind, A02Attr, A03Style,  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
